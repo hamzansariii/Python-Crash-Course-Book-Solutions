7ef1fbb1b4767d51b2b9baef4e76fe804adb6ff5
Hamza Ansari <91287876+Vidrow@users.noreply.github.com>
2022-01-25 01:10:18 +0530

    Delete Chapter_11 directory

--- Chapter_11/city_functions.py
+++
@@ -1,8 +0,0 @@
-
-
-def City_Country_Name(city_name, country_name, population=''):
-    if population:
-        name = f"{city_name},{country_name},{population}"
-    else:
-        name = f"{city_name},{country_name}"
-    return name
